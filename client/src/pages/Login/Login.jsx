397119731c02e1c9ec241b5879bfe47920151e23
unlin <unlinun@hotmail.com>
2023-01-18 23:52:54 +0800

    login form validation

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const navigate = useNavigate();
+  // form schema
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Wrong email format")
+      .required("Please provide email"),
+    password: yup.string().required("Please provide password"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const loginUser = async (data, e) => {
+    console.log(data);
+  };
+
   return (
     <div className="login">
       <h2>Login</h2>
-      <form className="form">
+      <form className="form" id="form" onSubmit={handleSubmit(loginUser)}>
         <div className="form__item">
           <label className="form__label">Email*</label>
           <input
@@ -13,8 +38,11 @@ const Login = () => {
             name="email"
             className="form__input"
             placeholder="example@example.com"
+            {...register("email")}
           />
-          <p className="form__alert form__alert--error"></p>
+          <p className="form__alert form__alert--error">
+            {errors?.email?.message}
+          </p>
         </div>
         <div className="form__item">
           <label className="form__label">Password*</label>
@@ -23,15 +51,16 @@ const Login = () => {
             name="password"
             className="form__input"
             placeholder="at least 6 character"
+            {...register("password")}
           />
-          <p className="form__alert"></p>
+          <p className="form__alert form__alert--error">
+            {errors?.password?.message}
+          </p>
         </div>
         <div className="form__submit">
-          <input
-            className="form__submit__btn"
-            type="submit"
-            value="Continue Login"
-          />
+          <button className="form__submit__btn" type="submit" form="form">
+            Continue Login
+          </button>
           <p>
             Try TiPro for free? <Link to="/register">Sign up here!</Link>
           </p>
