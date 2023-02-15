fff5a96b2754833256e61603476d6bd49325b6ff
unlin <unlinun@hotmail.com>
2023-02-15 12:26:15 +0800

    fix: fix staffs controller

--- server/controller/company.js
+++ server/controller/company.js
@@ -4,10 +4,13 @@ import User from "../models/User.js";
 // 取得公司員工資訊
 
 export const getStaffs = async (req, res) => {
-  const user = req.user;
+  const userId = req.user.id;
+  const user = await User.findOne({ _id: userId });
+  console.log(user);
   // 找尋包含自己的項目
   const staffs = await User.find({
     companyID: user.companyID,
   });
+  staffs.password = undefined;
   res.status(StatusCodes.OK).json({ staffs });
 };
