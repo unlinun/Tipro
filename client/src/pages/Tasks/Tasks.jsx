7de6faa342d65bfe92bfba092340c7f163f24540
unlin <unlinun@hotmail.com>
2023-02-15 12:16:59 +0800

    fix: fix layout

--- client/src/pages/Tasks/Tasks.jsx
+++ client/src/pages/Tasks/Tasks.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { useSelector } from "react-redux";
+
+import { getAllProjects } from "../../api/projects";
+import { getAllTasksByUser } from "../../api/task";
+import TaskTableRow from "./components/TaskTableRow";
 
 const Tasks = () => {
-  return <div>Tasks</div>;
+  const token = useSelector((state) => state.auth.token);
+
+  const { isLoading, data: tasks } = useQuery("tasksByUser", () =>
+    getAllTasksByUser(token)
+  );
+  const { data: projects } = useQuery("projects", () => getAllProjects(token));
+
+  return (
+    <div className="tasks">
+      <div className="tasks__card card">
+        <div className="card--flex card--flex--between mg__b--20">
+          <div className="card--flex">
+            <div className="card__title">tasks</div>
+          </div>
+          <div className="card__text card__text--sm">
+            total tasks : {tasks?.totalTasks}
+          </div>
+        </div>
+        <table className="tasks__table table">
+          <thead className="table__head">
+            <tr className="table__row table__row--head">
+              <th className="table__title">task title</th>
+              <th className="table__title">project</th>
+              <th className="table__title">phase</th>
+              <th className="table__title">start date</th>
+              <th className="table__title">due date</th>
+              <th className="table__title">tags</th>
+              <th className="table__title">status</th>
+              <th className="table__title">✓</th>
+              <th className="table__title"></th>
+            </tr>
+          </thead>
+          <tbody className="table__body">
+            {isLoading ? (
+              <tr style={{ textAlign: "center", justifySelf: "center" }}>
+                <td
+                  style={{
+                    textAlign: "center",
+                    margin: "12px",
+                  }}
+                >
+                  Loading...
+                </td>
+              </tr>
+            ) : null}
+            {tasks?.tasks.map((task) => {
+              return (
+                <TaskTableRow key={task?._id} task={task} projects={projects} />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
