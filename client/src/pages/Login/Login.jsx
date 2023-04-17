0ca66c0ff915b50da87e86a32dff0d5b3c5f1ef6
unlin <unlinun@hotmail.com>
2023-04-17 16:35:28 +0800

    fix:fix register companyID input

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -40,7 +40,6 @@ const Login = () => {
     try {
       const loginData = await loginAuth(data.email, data.password);
       // 如果登入成功，即可取得 token，並將 token 儲存於 redux 當中以便後續使用
-      console.log(loginData);
       if (loginData) {
         dispatch(
           setLogin({
