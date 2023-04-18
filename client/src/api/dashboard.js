9f13ef110b295ab9f1cce318f21f59b0072f6aa1
unlin <unlinun@hotmail.com>
2023-04-18 13:08:45 +0800

    fix: fix dashboard task's date

--- client/src/api/dashboard.js
+++ client/src/api/dashboard.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_BASE_URL;
+const API_URL = process.env.REACT_APP_BASE_URL
+  ? process.env.REACT_APP_BASE_URL
+  : "http://localhost:6001";
 
 export const getDashboard = async (token) => {
   const res = await axios.get(`${API_URL}/dashboard`, {
--- server/controller/dashboard.js
+++ server/controller/dashboard.js
@@ -1,17 +1,30 @@
 import { StatusCodes } from "http-status-codes";
-import NotFoundError from "../errors/notFound.js";
-import BadRequestError from "../errors/badRequest.js";
-import mongoose from "mongoose";
+
 import Projects from "../models/Projects.js";
 import Task from "../models/Task.js";
 import Memo from "../models/Memo.js";
 import User from "../models/User.js";
-import { getWeekEndDate, getWeekStartDate } from "../helper/date.js";
 
 export const getDashboard = async (req, res) => {
   const user = req.user;
-  const today = new Date(new Date().setHours(0, 0, 0, 0));
-  const endToday = new Date(new Date().setHours(23, 59, 59, 59));
+  const now = new Date(new Date().toLocaleDateString());
+  const today = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    0,
+    0,
+    0
+  );
+
+  const endToday = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1,
+    0,
+    0,
+    0
+  );
   const userCompany = await User.findOne({ _id: user.id });
   const projects = await Projects.find({
     $or: [{ manager: user.id }, { createdBy: user.id }, { staff: user.id }],
